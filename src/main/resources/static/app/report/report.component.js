efbceec04330f6e02c9685a208fbeab28a629298
Curtis Schlak <curtis.schlak@theironyard.com>
2017-06-27 14:20:56 -0500

    Update the report component to show how to render data.

--- src/main/resources/static/app/report/report.component.js
+++ src/main/resources/static/app/report/report.component.js
@@ -1,9 +1,29 @@
-class ReportController {}
+class ReportController {
+  constructor(clientsData, reportData) {
+    this.clientsData = clientsData;
+    this.reportData = reportData;
+  }
+  
+  $onInit() {
+    this.clientsData
+      .getAll()
+      .then(clients => this.clients = clients);
+  }
+  
+  generate() {
+    this.reportData
+      .create(this.selectedClient.id)
+      .then(data => this.data = data);
+  }
+}
 
 angular
   .module('app')
   .component('report', {
     templateUrl: '/app/report/report.component.html',
     controllerAs: 'report',
-    controller: [() => new ReportController()]
+    controller: [
+      'clientsData',
+      'reportData',
+      (clientsData, reportData) => new ReportController(clientsData, reportData)]
   });
